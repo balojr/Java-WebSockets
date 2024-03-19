995e1e15182eec6c113378a5495b23d58bc31ac1
balojr <martinkagendo13@gmail.com>
2024-03-19 13:20:18 +0300

    add stuff

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,52 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#message").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/message', function (greeting) {
+            showGreeting(JSON.parse(greeting.body));
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    var name = $("#name").val();
+    var message = { 'name': name };
+    stompClient.send("/app/message", {}, JSON.stringify(message));
+    showGreeting(message);
+}
+
+function showGreeting(message) {
+    $("#message").append("<tr><td>" + message.toString() + "</td></tr>");
+}
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
