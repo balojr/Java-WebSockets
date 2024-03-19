a3ac54d698544b8a6dc3006520a24f5ff4f93ed1
balojr <martinkagendo13@gmail.com>
2024-03-19 12:43:18 +0300

    add stuff

--- src/main/java/com/slime/websockets/WebSocketsApplication.java
+++ src/main/java/com/slime/websockets/WebSocketsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebSocketsApplication {
+public class WebsocketsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebSocketsApplication.class, args);
+		SpringApplication.run(WebsocketsApplication.class, args);
 	}
 
 }
